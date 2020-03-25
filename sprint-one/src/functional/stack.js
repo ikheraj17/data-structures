44eae3d1ce516998cd3500030b7baccbad3522af
Zachary Melman <54483906+zakLEGOmaniac@users.noreply.github.com>
2020-03-25 15:31:39 -0700

    function stack problem complete

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,41 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
+  //push adds a property to the someInstance object
   someInstance.push = function(value) {
+    //uses size as a key and the input value to create a new someInstance property.
+    storage[size] = value;
+    //the size variable is positioned to be the key for the next property
+    size ++;
   };
+
 
   someInstance.pop = function() {
+    //if size is greater than 0
+    if(size > 0){
+      //decrease size by 1
+      size --;
+      //create an alias to store the last key value pair
+      let current = storage[size];
+      //delets that key valeu pair from storage
+      delete storage[size];
+      //returns the key value pair
+      return current;
+    }
+
+
   };
 
+  // gets the current size of the someInstance object.
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
 };
+
+
+
+
