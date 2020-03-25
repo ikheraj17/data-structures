9a9085fba28340bc93589e3d377b0a7bcdcb1d70
Imran Kheraj <ikheraj17@gmail.com>
2020-03-25 17:31:59 -0500

    studying functions

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -13,6 +13,7 @@ var Queue = function() {
   };
 
   someInstance.size = function() {
+    
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,22 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
+    if(Object.keys(storage).length < 1){
+      return 0;
+    }else{
+      return Object.keys(storage).length;
+    }
   };
 
   return someInstance;
