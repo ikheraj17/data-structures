72f657b9f6ac3a422d09d85b8a9981262788686e
Imran Kheraj <ikheraj17@gmail.com>
2020-03-26 18:46:27 -0500

    prototypal class functions finished for queue and stack

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,40 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  return newQueue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enquesize : 0,
+  dqsize : 0,
+
+  enqueue : function(value) {
+    this[this.enquesize] = value;
+    this.enquesize ++;
+  },
+
+  dequeue : function() {
+    var deleted = this[this.dqsize];
+    delete this[this.dqsize];
+    this.dqsize ++;
+    return deleted;
+},
+
+  size: function(){
+    let arr = Object.keys(this);
+
+    if (arr.includes('enquesize') && arr.includes('dqsize')){
+      return Object.keys(this).length -2;
+    }
+    else if (arr.includes('enquesize') || arr.includes('dqsize')){
+      return Object.keys(this).length - 1;
+    }
+    else if (!arr.includes('enquesize') || !arr.includes('dqsize')){
+      return Object.keys(this).length;
+    }
+  }
+
+};
 
 
