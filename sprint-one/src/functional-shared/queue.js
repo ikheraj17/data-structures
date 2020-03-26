94bd5dd55266019ef2311135710c2685ba47afce
Imran Kheraj <ikheraj17@gmail.com>
2020-03-26 11:38:40 -0500

    Boiler plate functional shared structure for queue created

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {startingIndex : 0}
+  return _.extend(storage, queueMethods);
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue : function(value) {
+    
+  },
+
+  dequeue : function() {
+
+  }, 
+
+  size: function(){
+    return Object.keys(this).length - 4; 
+  }
+
+};
 
 
