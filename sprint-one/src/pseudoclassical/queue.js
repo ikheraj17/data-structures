bf0ac4230f0af086ebba66a213e420df652a62e7
Imran Kheraj <ikheraj17@gmail.com>
2020-03-26 19:34:35 -0500

    pseudoclassical Stack is finished, ccreated boiler plate functions for Queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.enqInd = 0;
+  this.dqInd = 0;
+  this.storage = ;
 };
+
+Stack.prototype.enqueue = function(value){
+  this.storage[this.size()] = value;
+}
+
+Stack.prototype.dequeue = function() {
+  var deleted = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return deleted;
+}
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
+}
 
 
