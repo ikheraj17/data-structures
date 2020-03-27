2f047dfd20aff591b64d98cc086648b6f5904325
Imran Kheraj <ikheraj17@gmail.com>
2020-03-26 20:37:21 -0500

    all functions finished

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,23 +1,40 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  this.enqInd = 0;
-  this.dqInd = 0;
-  this.storage = ;
+  //var newQueue = Object.create(queueMethods);
+  //return newQueue;
+  this.storage = {};
 };
 
-Stack.prototype.enqueue = function(value){
-  this.storage[this.size()] = value;
-}
 
-Stack.prototype.dequeue = function() {
-  var deleted = this.storage[this.size() - 1];
-    delete this.storage[this.size() - 1];
+  Queue.prototype.enquesize = 0,
+
+  Queue.prototype.dqsize = 0,
+
+  Queue.prototype.enqueue = function(value) {
+    this[this.enquesize] = value;
+    this.enquesize ++;
+  }
+
+  Queue.prototype.dequeue = function() {
+    var deleted = this[this.dqsize];
+    delete this[this.dqsize];
+    this.dqsize ++;
     return deleted;
 }
 
-Stack.prototype.size = function() {
-  return Object.keys(this.storage).length;
-}
+  Queue.prototype.size = function(){
+    let arr = Object.keys(this);
+
+    if (arr.includes('enquesize') && arr.includes('dqsize')){
+      return Object.keys(this).length -3;
+    }
+    else if (arr.includes('enquesize') || arr.includes('dqsize')){
+      return Object.keys(this).length - 2;
+    }
+    else if (!arr.includes('enquesize') || !arr.includes('dqsize')){
+      return Object.keys(this).length - 1;
+    }
+  }
 
 
