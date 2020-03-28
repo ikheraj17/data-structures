27ebbbbadeda2c811b08e74467a5606600779da5
Imran Kheraj <ikheraj17@gmail.com>
2020-03-27 21:46:15 -0500

    function Set completed

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+ //if this.storage does not have item
+ if(!this.contains(item)){
+   this.storage.push(item);
+ }
+ //push item
+
 };
 
 setPrototype.contains = function(item) {
+  let regex = RegExp(`${item}`);
+  let stringObj = JSON.stringify(this);
+  return regex.test(stringObj);
 };
 
 setPrototype.remove = function(item) {
+  for (let i = 0; i < this.storage.length; i ++){
+    let curr = this.storage[i];
+    if(curr === item){
+      this.storage.splice(i, 1);
+    }
+  }
 };
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
