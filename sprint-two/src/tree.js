a28bdaa64cdd617f43680fbe4e1b6509bfdf3374
Imran Kheraj <ikheraj17@gmail.com>
2020-03-27 20:53:41 -0500

    tree finished

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,30 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
+_.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+//create a new instance instance of Tree
+let treeChild = Tree(value);
+//place it in parent node newTree.children array
+//'this' refers to treeMethods, which does not possess the key "children"
+//this works because of _.extend, ehich takes methods from TreeMethods and applies
+//them to newTree. newTree posesses the key "children", and so the .push method
+//works and creates a whole new tree within newTree.
+//this is how a parent tree is extended
+this.children.push(treeChild)
 };
 
 treeMethods.contains = function(target) {
+  let regex = RegExp(`${target}`);
+  let stringObj = JSON.stringify(this);
+  return regex.test(stringObj);//<----stringified object
 };
 
 
